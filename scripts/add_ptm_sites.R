5f21950215af3781f5e02e04d60b1517ab2b17a3
khanspers <kristina.hanspers@gladstone.ucsf.edu>
2024-03-18 17:36:08 -0700

    alt strategy for script

--- scripts/add_ptm_sites.R
+++ scripts/add_ptm_sites.R
@@ -15,14 +15,18 @@ library(dplyr)
 library(stringr)
 cytoscapePing()
 
-#setwd("~/Dropbox (Gladstone)/Work/github/network-ptm-integration/scripts")
+setwd("~/Dropbox (Gladstone)/Work/github/network-ptm-integration/scripts")
 
 ###############
 ## Read in data
 
 ##Phosphoprotein data
 cptac.data <- read.csv("../datasets/PROGENy__EGFR.txt", stringsAsFactors = F, sep = "\t")
-cptac.data.brca.pval <- cptac.data %>% filter(BRCA.pval > 0 & BRCA.pval < 0.05) ##dataset specific: keep only rows with data. Change cancer type if necessary.
+cptac.data.brca.pval <- cptac.data %>% 
+  mutate(comp_id=paste0(symbol, "_", site)) %>%
+  filter(BRCA.pval > 0 & BRCA.pval < 0.05)
+
+##dataset specific: keep only rows with data. Change cancer type if necessary.
 # phospho.data <- read.csv("../datasets/Francavilla_PMID28355574_mmc4_phospho.csv", stringsAsFactors = F)
 
 ##Protein data. Rename columns.
@@ -40,30 +44,34 @@ psp.data.human<- psp.data %>% filter(KIN_ORGANISM == "human" & IN_VIVO_RXN == "X
 RCy3::commandsRun('wikipathways import-as-pathway id=WP4806') 
 mode <- "all"
 
-##Map all nodes to Uniprot. Skip this if mapping via symbol.
-#mapped.cols <- mapTableColumn('XrefId','Human','Ensembl','Uniprot', force.single=FALSE) ##doesn't work like expected
-
 ##Get all relevant data nodes in pathway
 node.table <- getTableColumns()
-node.table.prot <- subset(node.table, Type == 'Protein' | Type == 'GeneProduct') %>% select(SUID, name, XrefId, Ensembl) ##get node table entries for relevant nodes
+node.table.prot <- subset(node.table, Type == 'Protein' | Type == 'GeneProduct') %>%
+  select(SUID, name, XrefId, Ensembl) ##get node table entries for relevant nodes
 
 ##All matching nodes: Intersection between pathway protein/gene nodes and phospho data
-matching.nodes.prot <- node.table.prot %>% filter(name %in% cptac.data.brca.pval$symbol)
-matching.nodes.phospho <- cptac.data.brca.pval %>% filter(symbol %in% node.table.prot$name) %>% mutate(comp_id=paste0(symbol, "_", site)) %>% select(BRCA.val, BRCA.pval, symbol, site, comp_id)
+matching.nodes.prot <- node.table.prot %>% 
+  filter(name %in% cptac.data.brca.pval$symbol) %>% 
+  rename(SUID_prot = SUID) %>% 
+  rename(symbol = name) %>%
+  select(SUID_prot, symbol) #these are the nodes we are going to add sites to
 
-#cptac.data.ptm <- cptac.data.pval[cptac.data.pval$symbol %in% ptm.nodes, ] #Relevant subset of phospho data
-#phospho.nodes <- cptac.data.ptm$site 
+#need this for matching with kinase substrate data
+matching.nodes.phospho <- cptac.data.brca.pval %>%
+  filter(symbol %in% node.table.prot$name) %>%
+  mutate(comp_id=paste0(symbol, "_", site)) %>%
+  select(symbol, site, comp_id, BRCA.val, BRCA.pval)
+# 
+# matching.nodes.final <- merge(matching.nodes.prot, matching.nodes.phospho)
 
-source.target <- matching.nodes.phospho %>% select(symbol, comp_id)
-##Convert to list format that RCy3 expects
-source.target.list <- list() 
-for (i in 1:nrow(source.target)) {
-  row <- source.target[i, ]  
-  row_list <- as.list(row)  
-  source.target.list[[i]] <- row_list  
-}
+# summarized <- matching.nodes.final %>% group_by(SUID_prot) %>% 
+#   summarize(site = paste(sort(unique(site)),collapse=","))
 
+matching.nodes.prot.final <- merge(matching.nodes.prot, summarized)
+
+###############
 ##Only those with kinases on pathway: Subset of matching nodes with relevant kinases in the pathway
+##To-Do: need to refactor for alt strategy
 kinase.pw <- intersect(psp.data$GENE, node.table.prot$name) #Read in kinase-substrate data
 
 #create temp data frames for comparisons
@@ -73,73 +81,63 @@ psp.cptac.sub <- inner_join(psp.data.sub, cptac.data.sub) #combined
 psp.cptac.kin <- psp.cptac.sub[psp.cptac.sub$GENE %in% kinase.pw, ] #subset for only sites with kinases on the pathway
 
 #define phospho site nodes and edges
-#phospho.nodes.kin <- psp.cptac.kin$site just use psp.cptac.kin$comp_id instead
 source.target.kin <- psp.cptac.kin %>% select(symbol, comp_id)
 ptm.nodes.kin <- psp.cptac.kin$symbol
 
-###############
-##Adding phospho nodes to pathway
-
-##To-Do: Select mode
+#If mode is "kinase only", update inputs
 if (mode == "kinase"){
   phospho.nodes <- phospho.nodes.kin
   source.target <- source.target.kin
   ptm.nodes <- ptm.nodes.kin
 }
+###############
+##Adding phospho nodes to pathway
 
-##To-Do: Change this to use composite id to make it be always unique
-phospho.suid <- addCyNodes(node.names=matching.nodes.phospho$comp_id, skip.duplicate.names = FALSE) ##phospho sites are not unique
-#addCyNodes(phospho.nodes, skip.duplicate.names = FALSE) ##phospho sites are not unique
-addCyEdges(source.target.list)
-node.table.new <- getTableColumns() #get updated node table
-phospho.suid <- as.data.frame(do.call(rbind, phospho.suid))
-
-
-## Update ptm node position to be slightly offset from the parent node
-## To-Do: better logic for new node position, multiple nodes etc
-
-#Plan: Loop through all regular nodes. For each, lookup which phospho nodes belong, then adjust node position accordingly for each
-node.positions <- getNodePosition(node.names = matching.nodes.prot$SUID)
-node.positions <- cbind(SUID=rownames(node.positions), node.positions)
-
-#for (n in cptac.data.sub$symbol){
-for (n in matching.nodes.prot$SUID){
-  #define positions for phospho nodes based on x,y
-  pos.x <- node.positions$x_location[node.positions$symbol == n]
-  pos.y <- node.positions$y_location[node.positions$symbol == n]
-  positions.phospho <- data.frame(position=1:4, x=c(pos.x+30, pos.x+30, pos.x-30, pos.x-30), y=c(pos.y+10, pos.y-10, pos.y-10, pos.y+10))
-  # 
-  ##To-Do: complete this section using SUIDs
-  # #get phospho sites for this node and check how many. If more than 4, delete extras
-  # p.nodes <- matching.data.phospho %>% subset(symbol == n)
-  # if (length(p.nodes) > 4){
-  #   p.nodes <- p.nodes[1:4, ]
-  # }
-  # 
-  # pos <- 1
-  # #test <- subset(positions.phospho, position == "1")$x
-  # for (p in p.nodes){
-  #   x <- subset(positions.phospho, position == pos )$x
-  #   y <- subset(positions.phospho, position == pos)$y
-  #   setNodePositionBypass(p, x, y)
-  #   pos <- pos+1
-  # }
+##Alt strategy: Add phospho nodes by looping through protein nodes
+#p <- "38082" #testing
+for (p in matching.nodes.prot$SUID_prot){
+  prot <- matching.nodes.prot$symbol[matching.nodes.prot$SUID_prot == p]
+  #get node position for protein node
+  p.position <- getNodePosition(node.names = p) #requires node name
+  #calculate possible phospho site positions, limit to 4 sites
+  phospho.positions <- data.frame()
+  pos.x <- p.position$x_location
+  pos.y <- p.position$y_location
+  phospho.positions <- data.frame(position=1:4, x=c(pos.x+30, pos.x+30, pos.x-30, pos.x-30), y=c(pos.y+10, pos.y-10, pos.y-10, pos.y+10))
+  
+  #find relevant subset of cptac data
+  phospho.nodes <- cptac.data.brca.pval %>% 
+    filter(symbol == prot)
+  
+  #add a node for each
+  suids <- addCyNodes(node.names = phospho.nodes$comp_id)
+  #suids_2 <- as.data.frame(do.call(rbind, suids)) ##doesn't work
+  #addCyEdges()
+  
+  #set node position bypass for phospho nodes
+  pos <- 1
+  for (ptm in phospho.nodes$comp_id){
+    print(pos)
+    x <- subset(phospho.positions, position == pos)$x
+    y <- subset(phospho.positions, position == pos)$y
+    setNodePositionBypass(ptm, x, y)
+    pos <- pos+1
+  }
 }
 
 ##############
 ## Data viz
 style.name = "WikiPathways"
 
-#To-Do: use cancer-type specific data (pval) to create mapping
-#setNodeColorBypass
-
-#setNodeShapeBypass(phospho.node.names, 'ellipse')
-#setNodeColorMapping('AvgExp', colors=paletteColorBrewerRdBu, style.name=style.name)
+setNodeFontSizeBypass(matching.nodes.phospho$comp_id, 9)
+setNodeWidthBypass(matching.nodes.phospho$comp_id, 50)
+setNodeHeightBypass(matching.nodes.phospho$comp_id, 25)
 
 ## Update label for phospho nodes
-# phospho.nodes.position <- phospho.data.sub %>% pull(Aa.Position) %>% as.list()
-# selectNodes(phospho.node.names, by='name')
-# setNodeLabelBypass(phospho.node.names, phospho.data.sub[,c("Aa.Position")])
+#To-Do: update ptm node labels. Maybe read in a new column. 
+
+#To-Do: use cancer-type specific data (pval) to create mapping via comp_id
+#setNodeColorBypass
 
 ## Load protein data
 #loadTableData(protein.data.avgexp,data.key.column="Gene.names", "node", table.key.column = 'name')
