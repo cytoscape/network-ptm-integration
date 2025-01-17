52bb7a64f59468c25fe02c38a96e14b23fe81cc1
khanspers <kristina.hanspers@gladstone.ucsf.edu>
2025-01-17 15:11:55 -0800

    updates to use Ensprot id instead of symbol

--- scripts/add_ptm_sites.R
+++ scripts/add_ptm_sites.R
@@ -3,7 +3,7 @@ if (!requireNamespace("BiocManager", quietly = TRUE)){
   install.packages("BiocManager")}
 
 BiocManager::install("rWikiPathways")
-aBiocManager::install("RCy3")
+BiocManager::install("RCy3")
 #BiocManager::install("BridgeDbR")
 install.packages("dplyr")
 #install.packages("tidyverse")
@@ -17,7 +17,7 @@ library(purrr)
 cytoscapePing()
 
 ##Change working dir for local dev. Adjust path based on setup.
-setwd("~/Dropbox (Gladstone)/Work/github/network-ptm-integration/scripts")
+setwd("~/github/network-ptm-integration/scripts")
 
 ###############
 ## Read in data files
@@ -27,13 +27,13 @@ setwd("~/Dropbox (Gladstone)/Work/github/network-ptm-integration/scripts")
 ## Add unique id symbol_site for each protein/site combination
 cptac.phospho <- read.csv("../datasets/CPTAC_phospho_tn.txt", stringsAsFactors = F, sep = "\t")
 cptac.phospho <- cptac.phospho %>% 
-  mutate(symbol_site=paste0(symbol, "_", site))
+  mutate(prot_site=paste0(protein, "_", site))
 
 ## Extracting phospho data for one cancer type: CCRCC
 ## Select significant rows based on significant p-value in CCRCC vs control
 cptac.phospho.ccrcc.sig <- cptac.phospho %>% 
   filter(CCRCC.pval > 0 & CCRCC.pval < 0.05) %>%
-  select(symbol, site, protein, symbol_site, CCRCC.val, CCRCC.pval)
+  select(symbol, site, protein, prot_site, CCRCC.val, CCRCC.pval)
 
 ## PROGENy pathway correlated phospho data. PROGENy data for multiple pathways are available in datasets/PROGENy.
 ## Sites are either positively correlated with the pathway (contributing to pathway activity), or negatively correlated (inhibitory) 
@@ -45,14 +45,14 @@ cptac.progeny.egfr <- read.csv("../datasets/PROGENy/PROGENy__EGFR.txt", stringsA
 ## To-Do: Not sure how to best filter this data using val and pval
 cptac.progeny.egfr.ccrcc.pos <- cptac.progeny.egfr %>%
   filter(CCRCC.pval > 0 & CCRCC.pval < 0.05) %>%
-  mutate(symbol_site=paste0(symbol, "_", site)) %>% 
-  select(symbol, protein, site, symbol_site, CCRCC.val, CCRCC.pval) 
+  mutate(prot_site=paste0(protein, "_", site)) %>% 
+  select(symbol, protein, site, prot_site, CCRCC.val, CCRCC.pval)
 
 ## Negatively regulated PROGENy EGFR pathway and CCRCC
 cptac.progeny.egfr.ccrcc.neg <- cptac.progeny.egfr %>%
   filter(CCRCC.pval > -0.05 & CCRCC.pval < 0) %>%
-  mutate(symbol_site=paste0(symbol, "_", site)) %>%
-  select(symbol, protein, site, symbol_site, CCRCC.val, CCRCC.pval)
+  mutate(prot_site=paste0(protein, "_", site)) %>%
+  select(symbol, protein, site, prot_site, CCRCC.val, CCRCC.pval)
 
 ## Protein data from CPTAC. Note that this includes NA values.
 ## The "pval" and "val" statistics are from Wilcoxon rank sum test. Positive values means abundance is higher in tumor.
@@ -67,8 +67,8 @@ cptac.protein.ccrcc <- cptac.protein %>%
 psp.data <- read.csv("../annotations/Kinase_Substrate_Dataset.txt", stringsAsFactors = F, sep = "\t")
 psp.data.human<- psp.data %>%
   filter(KIN_ORGANISM == "human" & IN_VIVO_RXN == "X")  %>% 
-  mutate(symbol_site=paste0(SUBSTRATE, "_", SUB_MOD_RSD)) %>%
-  select(GENE, symbol_site, SUBSTRATE, SUB_MOD_RSD) #human only, in vivo evidence only
+  mutate(symbol_site=paste0(SUBSTRATE, "_", SUB_MOD_RSD)) %>% ##Update for protein instead of symbol. This requires mapping from UniProt id to Ensprot after this step to work with the rest of the script.
+  select(GENE, symbol_site, SUBSTRATE, SUB_MOD_RSD) #human only, in vivo evidence only. 
 
 ## Import BioMART mapping data and rename columns. To enable data mapping between node table and CPTAC data using EnsemblProt id.
 biomart <- read.csv("../annotations/mart_export.txt", stringsAsFactors = F, sep = ",")
@@ -97,13 +97,13 @@ node.table.prot <- subset(node.table, Type == 'Protein' | Type == 'GeneProduct')
 
 ## Add Ensembl prot column to use for all data mapping
 node.table.prot.mapped <- merge(node.table.prot, biomart, by="Ensembl") %>%
-  filter(!is.na(EnsemblProt)) ##this contains rows where EnsemblProt is empty
+  filter(EnsemblProt !="") ##this contains rows where EnsemblProt is empty
 
 ## Find pathway protein nodes to add phospho nodes to. Intersection between pathway protein/gene nodes and positively regulated PROGENy sites.
 ## Using PROGENy data, positive regulation
 matching.nodes.prot <- node.table.prot.mapped %>% 
   filter(EnsemblProt %in% cptac.progeny.egfr.ccrcc.pos$protein) %>% 
-  select(SUID, name) 
+  select(SUID, name, EnsemblProt)
 
 ##Get position for all relevant pathway protein nodes, store as data frame
 node.positions <- getNodePosition(node.names=matching.nodes.prot$SUID)
@@ -149,9 +149,9 @@ node.layout.pie <- node.layout.pie %>%
 
 ## Use PROGENy data to select ptms to add
 matching.nodes.phospho <- cptac.progeny.egfr.ccrcc.pos %>%
-  filter(symbol %in% node.table.prot$name) %>%
-  mutate(symbol_site=paste0(symbol, "_", site)) %>%
-  select(symbol, site, symbol_site)
+  filter(protein %in% node.table.prot.mapped$EnsemblProt) %>%
+  mutate(prot_site=paste0(protein, "_", site)) %>%
+  select(symbol, protein, site, prot_site) 
 
 ###############
 ## Kinase-restricted mode: Only those sites with kinases on pathway: Subset of matching nodes with relevant kinases in the pathway
@@ -184,15 +184,17 @@ if (mode == "kinase"){
 ptms.all <- data.frame()
 
 for (p in matching.nodes.prot$SUID){
-  prot <- matching.nodes.prot$name[matching.nodes.prot$SUID == p]
+  prot <- matching.nodes.prot$EnsemblProt[matching.nodes.prot$SUID == p] 
+  print(prot)
   #find relevant subset of cptac phospho data, these are the ptm nodes to add
   phospho.nodes <- site.table %>% 
-    filter(symbol == prot) %>% 
-    select(symbol_site)
+    filter(protein == prot) %>% 
+    select(prot_site)
   phospho.nodes <- head(phospho.nodes, 4) ##take first 4
   
   ##Add a node for each ptm, for the particular protein. Collect the SUID for the added ptm, and use it for updating position below.
-  suids <- addCyNodes(node.names = phospho.nodes$symbol_site, skip.duplicate.names = FALSE)
+  suids <- addCyNodes(node.names = phospho.nodes$prot_site, skip.duplicate.names = FALSE)
+  print(suids)
   ptms <- data.frame()
 
   for (l in suids){
@@ -202,7 +204,6 @@ for (p in matching.nodes.prot$SUID){
   ##Set node position bypass for ptm nodes.
   pos <- 1
   for (ptm in ptms$SUID){
-    #addCyEdges(c(p, ptm)) #need the SUID for this
     x <- node.layout$x.ptm[node.layout$suid == p][[1]][[pos]]
     y <- node.layout$y.ptm[node.layout$suid == p][[1]][[pos]]
       setNodePositionBypass(ptm, x, y)
@@ -229,9 +230,9 @@ setNodeHeightBypass(ptms.all$SUID, 20)
 ## For this we can use the same visualization since the value is the same, Wilcoxon rank sum test
 
 loadTableData(cptac.protein.ccrcc, data.key.column="ensembl", "node", table.key.column = 'Ensembl') ##load protein data
-loadTableData(cptac.phospho.ccrcc.sig, data.key.column="symbol_site", "node", table.key.column = 'shared name') ##load phospho data
+loadTableData(cptac.phospho.ccrcc.sig, data.key.column="prot_site", "node", table.key.column = 'shared name') ##load phospho data
 RCy3::setNodeColorMapping('CCRCC.val', colors=paletteColorBrewerRdBu, style.name = style.name) 
-ptms.all.data <- inner_join(ptms.all, cptac.phospho, by = join_by(name == symbol_site)) ##add back site info etc
+ptms.all.data <- inner_join(ptms.all, cptac.phospho, by = join_by(name == prot_site)) ##add back site info etc
 
 ##Create new df to map to network to update ptm node label
 ptms.all.data.site <- ptms.all.data %>%
@@ -253,7 +254,7 @@ cptac.phospho.ccrcc.ov <- cptac.phospho.ccrcc.sig %>%
   mutate(symbol_ptm=paste0(symbol, "_ptm"))
 
 # write to file since no option to import data frame directly
-#write.table(cptac.phospho.ccrcc.ov,"/Applications/Cytoscape_v3.10.2/sampleData/cptac.phospho.ccrcc.sig.txt",sep="\t",row.names=FALSE)
+write.table(cptac.phospho.ccrcc.ov,"/Applications/Cytoscape_v3.10.2/sampleData/cptac.phospho.ccrcc.sig.txt",sep="\t",row.names=FALSE)
 
 #make new data frame with correct name
 matching.nodes.prot.pie <- node.table.prot.mapped %>% 
