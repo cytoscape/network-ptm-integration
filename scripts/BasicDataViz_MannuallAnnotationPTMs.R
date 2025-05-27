cfeb505a42e44403a90e6334e6e43236280aefd9
khanspers <kristina.hanspers@gladstone.ucsf.edu>
2025-05-27 11:34:18 -0700

    renamed script, updated comments

--- scripts/BasicDataViz_MannuallAnnotationPTMs.R
+++ scripts/BasicDataViz_MannuallAnnotationPTMs.R
@@ -1,9 +1,9 @@
 ###############
 
-## This script demonstrates how to perform basic data visualization on manually cureated ptms on WikiPathways models.
+## This script demonstrates how to perform basic data visualization in Cytoscape on manually curated ptms on WikiPathways models.
 ## The example data files are from CPTAC (https://proteomics.cancer.gov/programs/cptac) and use Ensembl Protein (peptide) identifiers.
 ## Adjustments to the script will need to be made for data with other identifier types, like Uniprot.
-## The strategy is to visualize the same data type, "val" for both proteomics data and phosphoproteomics data on the main data node 
+## The strategy is to visualize the same data type, "val" in the example data, for both proteomics data and phosphoproteomics data on the main data node 
 ## and the ptm "state" node respectively.
 
 ###############
@@ -22,23 +22,26 @@ library(rWikiPathways)
 library(RCy3)
 library(dplyr)
 library(stringr)
+
+# Ensure Cytoscape is running and connected
 cytoscapePing()
 
-## Change working dir for local dev. Adjust path based on setup.
+## Change working dir. Adjust path based on setup.
 setwd("~/github/network-ptm-integration/scripts")
 
 ###############
 ## Read in data files. The example data files are from CPTAC (https://proteomics.cancer.gov/programs/cptac). 
 
-## Phosphoprotein example from CPTAC, for Clear cell renal cell carcinoma (CCRCC) vs normal
+## Phosphoproteomics data from CPTAC, for Clear cell renal cell carcinoma (CCRCC) vs normal
 ## The "pval" and "val" statistics are from Wilcoxon rank sum test. Positive values means abundance is higher in tumor.
 cptac.phospho <- read.csv("../datasets/CPTAC_phospho_tn.txt", stringsAsFactors = F, sep = "\t")
 
-## Protein data from CPTAC, for Clear cell renal cell carcinoma (CCRCC) vs normal. 
+## Proteomics data from CPTAC, for Clear cell renal cell carcinoma (CCRCC) vs normal. 
 ## The "pval" and "val" statistics are from Wilcoxon rank sum test. Positive values means abundance is higher in tumor.
 cptac.protein <- read.csv("../datasets/CPTAC_protein_tn.txt", stringsAsFactors = F, sep = "\t")
 
 ## Get BioMART mapping info for Ensembl protein id to Uniprot-Swissprot, to enable data mapping between node table and CPTAC data using EnsemblProt id.
+## Manually annotated ptms on pathways are annotated with Uniprot-Swissprot identifiers.
 ensembl = useEnsembl(biomart="ensembl", dataset="hsapiens_gene_ensembl")
 ensemblprot_swissprot <- getBM(attributes=c('ensembl_peptide_id','hgnc_symbol','uniprotswissprot'), mart = ensembl)
 ensemblprot_swissprot_sub <- ensemblprot_swissprot %>%
@@ -47,17 +50,17 @@ ensemblprot_swissprot_sub <- ensemblprot_swissprot %>%
 ensemblprot_swissprot_sub <- ensemblprot_swissprot_sub[, c("ensembl_peptide_id", "uniprotswissprot")]
 
 ###############
-## Open and process WP, get relevant phospho data nodes
-## Open the relevant WP. The below example uses the EGFR pathway.
+## Open the relevant WP in Cytoscape. The below example uses the EGFR pathway.
 RCy3::commandsRun('wikipathways import-as-pathway id=WP4806') 
 
-## Get all relevant protein/gene product data nodes in pathway
+## Get the full node table for the pathway.
 node.table <- RCy3::getTableColumns(table = "node")
-## Get the ptm nodes. This will be used later for manipulating the visual style of ptm nodes
+## Get the ptm nodes. This will be used later for manipulating the visual style of ptm nodes.
 ptm.nodes <- node.table %>%
   filter(ptm == 'p')
 
-## Add new column, data_mapping_id, for data mapping.
+## Add a new column, data_mapping_id, for data mapping. A new data frame is created with the new column and then read into the Cytoscape node table.
+## The new data mapping column will contain a new id which is a composite of the parent id (Uniprot) and ptm site information, for example P27361_thr202.
 node.table.ptm <- node.table %>% 
   filter(ptm == 'p') %>%
   mutate(data_mapping_id=paste0(parentid, "_", position)) %>% 
@@ -66,7 +69,7 @@ node.table.ptm <- node.table %>%
 
 loadTableData(node.table.ptm, data.key.column="SUID", "node", table.key.column = 'SUID') ## load the new column into Cytoscape
 
-## Add Ensembl ids to data_mapping_id column
+## Add Ensembl ids to data_mapping_id column. For the proteomics data, this will be the mapping id. We are simply moving it from the Ensembl column to the data_mapping_id
 node.table.ensembl <- node.table %>% 
   filter(Type == 'GeneProduct') %>%
   mutate(data_mapping_id=Ensembl) %>% 
@@ -74,16 +77,17 @@ node.table.ensembl <- node.table %>%
 
 loadTableData(node.table.ensembl, data.key.column="SUID", "node", table.key.column = 'SUID') ## load the new column into Cytoscape
 
-## Map phosphoproteomics data from Ensembl Prot ids to Swissprot using Biomart data mapping
+## Map phosphoproteomics data from Ensembl Prot ids to Swissprot using Biomart data mapping.
+## The proteomics data has the ptm site information in a different format, i.e. S233 vs ser233, so we need to translate that as well to match the ptms in the network.
 cptac.phospho.mapped <- merge(cptac.phospho, ensemblprot_swissprot_sub, by.x = "protein", by.y = "ensembl_peptide_id") %>%
   mutate(site = str_replace(site, "^S", "ser"),
          site = str_replace(site, "^T", "thr"),
          site = str_replace(site, "^Y", "tyr")) %>%
   mutate(prot_site=paste0(uniprotswissprot, "_", site))
 
-## Load data
-loadTableData(cptac.phospho.mapped, data.key.column="prot_site", "node", table.key.column = 'data_mapping_id') ##load phospho data
-loadTableData(cptac.protein, data.key.column="ensembl", "node", table.key.column = 'data_mapping_id') ##load phospho data
+## Load phosphoproteomics and proteomics data into Cytoscape, using the newly added data_mapping_id column.
+loadTableData(cptac.phospho.mapped, data.key.column="prot_site", "node", table.key.column = 'data_mapping_id') 
+loadTableData(cptac.protein, data.key.column="ensembl", "node", table.key.column = 'data_mapping_id') 
 
 ## Set visual style
 style.name = "WikiPathways"
